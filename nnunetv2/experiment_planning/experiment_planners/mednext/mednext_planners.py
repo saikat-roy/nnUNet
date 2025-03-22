6a9f8945f430b210d74a366c888bef74931a87ac
Roy, Saikat <saikat.roy@dkfz-heidelberg.de>
2025-03-22 09:54:03 +0100

    added planners for mednext

---
+++ nnunetv2/experiment_planning/experiment_planners/mednext/mednext_planners.py
@@ -0,0 +1,55 @@
+from nnunetv2.experiment_planning.experiment_planners.default_experiment_planner import ExperimentPlanner
+
+class MedNeXtPlanner(ExperimentPlanner):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def plan_experiment(self, *args, **kwargs):
+        # Call the parent method to get the standard nnUNet planning
+        plans = super().plan_experiment(*args, **kwargs)
+        
+        # Modify the patch size and batch size in the plan
+        plans['configurations']['3d_fullres_mednext'] = {
+                    'inherits_from': '3d_fullres',
+                    'patch_size': (128, 128, 128),
+                    'batch_size': 2,
+                }
+
+        self.save_plans(plans)
+        return plans
+    
+
+class MedNeXtPlannerDDP(ExperimentPlanner):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def plan_experiment(self, *args, **kwargs):
+        # Call the parent method to get the standard nnUNet planning
+        plans = super().plan_experiment(*args, **kwargs)
+        
+        # Modify the patch size and batch size in the plan for DDP training.
+        # Designed for 8x A100 GPUs
+        plans['configurations']['3d_fullres_mednext_ddp_16'] = {
+                    'inherits_from': '3d_fullres',
+                    'patch_size': (128, 128, 128),
+                    'batch_size': 16,
+                }
+        
+        # Designed for 4x A100 GPUs
+        plans['configurations']['3d_fullres_mednext_ddp_16'] = {
+                    'inherits_from': '3d_fullres',
+                    'patch_size': (128, 128, 128),
+                    'batch_size': 8,
+                }
+        
+        # Designed for 2x A100 GPUs
+        plans['configurations']['3d_fullres_mednext_ddp_16'] = {
+                    'inherits_from': '3d_fullres',
+                    'patch_size': (128, 128, 128),
+                    'batch_size': 4,
+                }
+
+        self.save_plans(plans)
+        return plans

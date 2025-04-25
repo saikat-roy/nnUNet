6a970b208eb90493a1caea0341ae51c4731b554f
Yannick Kirchhoff <yannick.kirchhoff@dkfz-heidelberg.de>
2025-04-25 17:29:01 +0200

    adjust inference script for AbdomenAtlas1.1

--- nnunetv2/inference/aa11_inference.py
+++ nnunetv2/inference/aa11_inference.py
@@ -11,15 +11,17 @@ from time import sleep
 from warnings import warn
 
 from batchgenerators.dataloading.multi_threaded_augmenter import MultiThreadedAugmenter
-from batchgenerators.utilities.file_and_folder_operations import maybe_mkdir_p, isdir, load_json, save_pickle, join
+from batchgenerators.utilities.file_and_folder_operations import maybe_mkdir_p, isdir, load_json, join
 
 from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
-from nnunetv2.inference.export_prediction import convert_predicted_logits_to_segmentation_with_correct_shape
 from nnunetv2.configuration import default_num_processes
 from nnunetv2.inference.sliding_window_prediction import compute_gaussian
 from nnunetv2.utilities.file_path_utilities import check_workers_alive_and_busy
 from nnunetv2.utilities.helpers import empty_cache
 from nnunetv2.utilities.plans_handling.plans_handler import PlansManager, ConfigurationManager
+from nnunetv2.utilities.label_handling.label_handling import LabelManager
+from nnunetv2.preprocessing.resampling.resample_torch import resample_torch_fornnunet
+from acvl_utils.cropping_and_padding.bounding_boxes import insert_crop_into_image
 
 
 label_mapping = {
@@ -51,6 +53,67 @@ label_mapping = {
 }
 
 
+def convert_predicted_logits_to_segmentation_with_correct_shape_custom(predicted_logits: Union[torch.Tensor, np.ndarray],
+                                                                plans_manager: PlansManager,
+                                                                configuration_manager: ConfigurationManager,
+                                                                label_manager: LabelManager,
+                                                                properties_dict: dict,
+                                                                return_probabilities: bool = False,
+                                                                num_threads_torch: int = default_num_processes):
+    old_threads = torch.get_num_threads()
+    torch.set_num_threads(num_threads_torch)
+
+    # resample to original shape
+    spacing_transposed = [properties_dict['spacing'][i] for i in plans_manager.transpose_forward]
+    current_spacing = configuration_manager.spacing if \
+        len(configuration_manager.spacing) == \
+        len(properties_dict['shape_after_cropping_and_before_resampling']) else \
+        [spacing_transposed[0], *configuration_manager.spacing]
+    # switch to torch resampling to avoid spikes in RAM
+    predicted_logits = resample_torch_fornnunet(predicted_logits,
+                                            properties_dict['shape_after_cropping_and_before_resampling'],
+                                            current_spacing,
+                                            [properties_dict['spacing'][i] for i in plans_manager.transpose_forward])
+    # return value of resampling_fn_probabilities can be ndarray or Tensor but that does not matter because
+    # apply_inference_nonlin will convert to torch
+    if not return_probabilities:
+        # this has a faster computation path becasue we can skip the softmax in regular (not region based) trainig
+        segmentation = label_manager.convert_logits_to_segmentation(predicted_logits)
+    else:
+        predicted_probabilities = label_manager.apply_inference_nonlin(predicted_logits)
+        segmentation = label_manager.convert_probabilities_to_segmentation(predicted_probabilities)
+    del predicted_logits
+
+    # put segmentation in bbox (revert cropping)
+    segmentation_reverted_cropping = np.zeros(properties_dict['shape_before_cropping'],
+                                              dtype=np.uint8 if len(label_manager.foreground_labels) < 255 else np.uint16)
+    segmentation_reverted_cropping = insert_crop_into_image(segmentation_reverted_cropping, segmentation, properties_dict['bbox_used_for_cropping'])
+    del segmentation
+
+    # segmentation may be torch.Tensor but we continue with numpy
+    if isinstance(segmentation_reverted_cropping, torch.Tensor):
+        segmentation_reverted_cropping = segmentation_reverted_cropping.cpu().numpy()
+
+    # revert transpose
+    segmentation_reverted_cropping = segmentation_reverted_cropping.transpose(plans_manager.transpose_backward)
+    if return_probabilities:
+        # revert cropping
+        predicted_probabilities = label_manager.revert_cropping_on_probabilities(predicted_probabilities,
+                                                                                 properties_dict[
+                                                                                     'bbox_used_for_cropping'],
+                                                                                 properties_dict[
+                                                                                     'shape_before_cropping'])
+        predicted_probabilities = predicted_probabilities.cpu().numpy()
+        # revert transpose
+        predicted_probabilities = predicted_probabilities.transpose([0] + [i + 1 for i in
+                                                                           plans_manager.transpose_backward])
+        torch.set_num_threads(old_threads)
+        return segmentation_reverted_cropping, predicted_probabilities
+    else:
+        torch.set_num_threads(old_threads)
+        return segmentation_reverted_cropping
+
+
 def export_prediction_from_logits_custom(predicted_array_or_file: Union[np.ndarray, torch.Tensor], properties_dict: dict,
                                   configuration_manager: ConfigurationManager,
                                   plans_manager: PlansManager,
@@ -62,7 +125,7 @@ def export_prediction_from_logits_custom(predicted_array_or_file: Union[np.ndarr
         dataset_json_dict_or_file = load_json(dataset_json_dict_or_file)
 
     label_manager = plans_manager.get_label_manager(dataset_json_dict_or_file)
-    ret = convert_predicted_logits_to_segmentation_with_correct_shape(
+    ret = convert_predicted_logits_to_segmentation_with_correct_shape_custom(
         predicted_array_or_file, plans_manager, configuration_manager, label_manager, properties_dict,
         return_probabilities=save_probabilities, num_threads_torch=num_threads_torch
     )
